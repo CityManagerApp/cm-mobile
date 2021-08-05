f7eb4a9a36ff825aabdb77ac4cbcfcb459036cbf
timurnugaev <decter49pro@gmail.com>
2021-08-05 23:14:30 +0300

    добавил фоткание фоток и их кроппинг

--- lib/page/add_photos_page.dart
+++ lib/page/add_photos_page.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:clik/widget/button_widget.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 import '../main.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:clik/network.dart';
+
+import 'package:image_picker/image_picker.dart';
 
 class AddPhotosPage extends StatefulWidget {
   @override
@@ -16,6 +20,33 @@ class AddPhotosPage extends StatefulWidget {
 }
 
 class _AddPhotosPage extends State<AddPhotosPage> {
+  File _selectedFile;
+
+  getImage(ImageSource src) async {
+    File img = await ImagePicker.pickImage(source: src);
+
+    if (img != null) {
+      File cropped = await ImageCropper.cropImage(
+        sourcePath: img.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+        compressQuality: 100,
+        maxWidth: 700,
+        maxHeight: 700,
+        compressFormat: ImageCompressFormat.jpg,
+        androidUiSettings: AndroidUiSettings(
+          toolbarColor: Colors.deepPurple,
+          toolbarTitle: "Выделите только необходимое",
+          statusBarColor: Colors.deepOrange.shade900,
+          backgroundColor: Colors.white,
+        ),
+      );
+
+      this.setState(() {
+        _selectedFile = cropped;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +64,50 @@ class _AddPhotosPage extends State<AddPhotosPage> {
                 fontSize: 28,
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            OutlineButton(
+              onPressed: () {
+                print('Сфоткать');
+                getImage(ImageSource.camera);
+              },
+              padding: EdgeInsets.all(10.0),
+              borderSide: BorderSide(color: Colors.blue),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(6.0)),
+              ),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                children: <Widget>[
+                  Icon(Icons.edit_outlined, color: Colors.blue),
+                  SizedBox(height: 4),
+                  Text("Сфоткать",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      )),
+                ],
+              ),
+            ),
+            OutlineButton(
+              onPressed: () {
+                print('С галереи');
+                getImage(ImageSource.camera);
+              },
+              padding: EdgeInsets.all(10.0),
+              borderSide: BorderSide(color: Colors.blue),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(6.0)),
+              ),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                children: <Widget>[
+                  Icon(Icons.edit_outlined, color: Colors.blue),
+                  SizedBox(height: 4),
+                  Text("С галереи",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      )),
+                ],
               ),
             ),
           ],
