d8bbbd36e34fd5af5f0a2b038abb124a54136bf9
timurnugaev <decter49pro@gmail.com>
2021-08-19 15:14:20 +0300

    permission[2]

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'ЦЛИК (v0.4)';
+  static final String title = 'ЦЛИК (v0.5)';
 
   @override
   Widget build(BuildContext context) => MaterialApp(
